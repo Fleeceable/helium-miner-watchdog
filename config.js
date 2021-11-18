1d46ba74b6084374341f73e5b292d74b883abe1f
Fleeceable <90242002+Fleeceable@users.noreply.github.com>
2021-11-19 00:07:24 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,78 @@
+{
+    "TelegramSettings": [
+        {
+            "token" : "1923erwrwr232AE0AXUPZ2342234tWvsArI3Wj0SUjw",
+            "chatId" : "12342343060" 
+        }
+    ],
+    "Miners": [
+        {
+            "MinerManufacture": "Nebra",
+            "MinerNickname": "[Home]",
+            "PublicAddress": "112resfsgWf59QVG6eBf87LCtetre7J9QQc95",
+            "MinerLocalIP": "192.168.1.50",
+            "MinerWatchdog": "true",
+            "RewardCheck": "true"
+        },
+        {
+            "MinerManufacture": "Nebra",
+            "MinerNickname": "[Friend1]",
+            "PublicAddress": "112ewr345t5egeg45gy3TgYKSEqP9",
+            "MinerLocalIP": "192.168.2.50",
+            "MinerWatchdog": "true",
+            "RewardCheck": "true"
+        },
+        {
+            "MinerManufacture": "Nebra",
+            "MinerNickname": "[Friend3]",
+            "PublicAddress": "112tergsd454y54erhrhrhSDsRzDC2zQu",
+            "MinerLocalIP": "84.22.115.24:8123",
+            "MinerWatchdog": "false",
+            "RewardCheck": "true"
+        },
+        {
+            "MinerManufacture": "Nebra",
+            "MinerNickname": "[Friend4]",
+            "PublicAddress": "112KEmiewtgth656tyjdfhe6erGXMhX",
+            "MinerLocalIP": "domain.ddnsservice.com:7392",
+            "MinerWatchdog": "false",
+            "RewardCheck": "true"
+        },
+        {
+            "MinerManufacture": "Heltec",
+            "MinerNickname": "[Office]",
+            "PublicAddress": "113d9y43tergdfghht45345aXh6xgeHzkxByGRM",
+            "MinerLocalIP": "192.168.3.50",
+            "MinerAuthorizationKey": "SFgfgsdghDGH459sgmeuerf=",
+            "MinerWatchdog": "true",
+            "RewardCheck": "true"
+        },
+        {
+            "MinerManufacture": "Heltec",
+            "MinerNickname": "[Tower]",
+            "PublicAddress": "1143twgeh536u3434restdhrtdh43yzkFKtkYX",
+            "MinerLocalIP": "192.168.4.50",
+            "MinerAuthorizationKey": "S43t34tergrshe63A=treh",
+            "MinerWatchdog": "true",
+            "RewardCheck": "true"
+        }
+    ],
+    "Accounts": [
+        {
+            "AccountNickname": "[Mine]", 
+            "PublicAddress": "ggwertg234thNSG2EXiGuox4qkfcWNJmHmt23tAeSxEE"
+        },
+        {
+            "AccountNickname": "[Friend1]", 
+            "PublicAddress": "32trewtpRD393xPbH34t234pCJDnv9TbSoBYdqwbqwerz"
+        }
+    ],
+    "TimeSettings": [
+        {
+            "BlockHeightDifference": 15,
+            "MinerCheckTime": 5,
+            "MinerDoubleCheckCount": 3,
+            "RewardCheckTime": 3
+        }
+    ]
+}
